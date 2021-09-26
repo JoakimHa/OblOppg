434bef8cfeda2e138c485ffed59351e886cc509f
JoakimHa <joakimha@getacademy.no>
2021-09-26 19:41:51 +0200

    oppdatert

--- testscript.js
+++ testscript.js
@@ -2,34 +2,36 @@
                 // controller 
         function select(barNo){
           
-             if (chosenBar == barNo) {
+      //     if (chosenBar == barNo) {
                chosenBar = 'ingen';
-           } else {
-            chosenBar = barNo;
+         //  } else {
+        }
+        function testselect() {
+          //  chosenBar = barNo;
             console.log(chosenBar);
             barArrayNR = barNo -1;
             console.log(barArrayNR);
-           }
+        //   }
             // let list = [];
              //list.
         }
         function change(){
-            if (inputValue < 0.1) {
-                alert('Error, Value has to be higher than "1"')
-            }else if (inputValue > 10) {
-            alert ('error, value too high correct value is 1-10')
-            } 
-            else {
+           // if (inputValue < 0.1) {
+           //     alert('Error, Value has to be higher than "1"')
+          //  }else if (inputValue > 10) {
+          //  alert ('error, value too high correct value is 1-10')
+           // } 
+           // else {
                 numbers.splice(barArrayNR, 1, inputValue);
             }
-        }
+       // }
         function addBar() {
             if (inputValue > 10) {
-                alert('error, value too high correct value is 1-10')
+             //   alert('error, value too high correct value is 1-10')
             }else if (inputValue >= 0.1) {
                 numbers.push(inputValue);
             } else {
-                alert ('Error, Value must be higher than "1"');
+             //   alert ('Error, Value must be higher than "1"');
             }
         }
         function deleteBar(){
